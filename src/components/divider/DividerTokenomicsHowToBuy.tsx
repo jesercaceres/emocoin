4c5ab9f61c6e972e56b9e5477072c1fe6926b470
Jéser Cáceres Marcelino <jeser.caceres01@yahoo.com>
2025-06-09 22:00:22 -0300

    feat: add powerglitch dependency and implement glitch effect in DividerTokenomicsHowToBuy component

--- src/components/divider/DividerTokenomicsHowToBuy.tsx
+++ src/components/divider/DividerTokenomicsHowToBuy.tsx
@@ -1,8 +1,15 @@
 // DividerTokenomicsHowToBuy.tsx
 import './DividerTokenomicsHowToBuy.css';
 import VhsOverlaySlender from '../VhsOverlay';   // caminho relativo
+import { PowerGlitch } from 'powerglitch'
+
+import { useEffect } from 'react';
 
 export default function DividerTokenomicsHowToBuy() {
+  useEffect(() => {
+    PowerGlitch.glitch('.divider-text');
+  }, []);
+
   return (
     <section className="divider-tokenomics">
       <VhsOverlaySlender />        {/* overlay WebGL + glitch */}
